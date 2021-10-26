6969ab12f931887caf4b954cf0815d9a692080a0
Paulo <paulobiscocho@gmail.com>
2021-10-26 12:16:55 +0800

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,73 +5,75 @@ import calculateWinner from 'helpers/CalculateWinner';
 import isBoardFull from 'helpers/IsBoardFull';
 
 export default function Game() {
-    const [ squares, setSquares ] = useState(Array(9).fill(null));
-    const [ isXNext, setIsXNext ] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [isXNext, setIsXNext] = useState(true);
     const nextSymbol = isXNext ? "X" : "O";
     const winner = calculateWinner(squares);
-  
+
     function getStatus() {
-      if (winner) {
-        return "Winner: " + winner;
-      } else if (isBoardFull(squares)) {
-        return "Draw!";
-      } else {
-        return "Next player: " + nextSymbol;
-      }
+        if (winner) {
+            return "Winner: " + winner;
+        } else if (isBoardFull(squares)) {
+            return "Draw!";
+        } else {
+            return "Next player: " + nextSymbol;
+        }
     }
-  
-    function renderSquare(i:number) {
-      return (
-        <Square
-          value={squares[i]}
-          onClick={() => {
-            if (squares[i] != null || winner != null) {
-              return;
-            }
-            const nextSquares = squares.slice();
-            nextSquares[i] = nextSymbol;
-            setSquares(nextSquares);
-  
-            setIsXNext(!isXNext); // toggle turns
-          }}
-        />
-      );
+
+    function renderSquare(i: number) {
+        return (
+            <Square
+                value={squares[i]}
+                onClick={() => {
+                    if (squares[i] != null || winner != null) {
+                        return;
+                    }
+                    const nextSquares = squares.slice();
+                    nextSquares[i] = nextSymbol;
+                    setSquares(nextSquares);
+
+                    setIsXNext(!isXNext); // toggle turns
+                }}
+            />
+        );
     }
-  
+
     function renderRestartButton() {
-      return (
-        <Restart
-          onClick={() => {
-            setSquares(Array(9).fill(null));
-            setIsXNext(true);
-          }}
-        />
-      );
+        return (
+            <Restart
+                onClick={() => {
+                    setSquares(Array(9).fill(null));
+                    setIsXNext(true);
+                }}
+            />
+        );
     }
-  
+
     return (
-      <div className="container">
-        <div className="game">
-          <div className="game-board">
-            <div className="board-row">
-              {renderSquare(0)}
-              {renderSquare(1)}
-              {renderSquare(2)}
+        <div className="container">
+            <div className="game">
+
+                <div className="game-info">TIC TAC TOE</div>
+                <div className="game-board">
+                    <div className="board-row">
+                        {renderSquare(0)}
+                        {renderSquare(1)}
+                        {renderSquare(2)}
+                    </div>
+                    <div className="board-row">
+                        {renderSquare(3)}
+                        {renderSquare(4)}
+                        {renderSquare(5)}
+                    </div>
+                    <div className="board-row">
+                        {renderSquare(6)}
+                        {renderSquare(7)}
+                        {renderSquare(8)}
+                    </div>
+                </div>
+                <div className="game-info">{getStatus()}</div>
+                <div className="restart-button">{renderRestartButton()}</div>
             </div>
-            <div className="board-row">
-              {renderSquare(3)}
-              {renderSquare(4)}
-              {renderSquare(5)}
-            </div>
-            <div className="board-row">
-              {renderSquare(6)}
-              {renderSquare(7)}
-              {renderSquare(8)}
-            </div>
-          </div>
-          <div className="game-info">{getStatus()}</div>
-          <div className="restart-button">{renderRestartButton()}</div>
         </div>
-      </div>
     );
-  }
+}
